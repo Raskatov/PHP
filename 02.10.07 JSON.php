b8bd140b6eea76ef3d464684bcfd6f680f2c1d0a
Alexandr <dcl1704@yandex.ru>
2018-03-03 12:00:59 +0400

    Create 02.10.07 JSON.php

---
+++ 02.10.07 JSON.php
@@ -0,0 +1,12 @@
+<?php
+
+//Парсинг JSON
+json_decode
+mixed json_decode ( string $json [, bool $assoc = false [, int $depth = 512 [, int $options = 0 ]]] )
+/*Принимает закодированную в JSON строку и преобразует ее в переменную PHP.*/
+
+
+//Кодирование в JSON
+json_encode
+string json_encode ( mixed $value [, int $options = 0 [, int $depth = 512 ]] )
+/*Возвращает строку, содержащую JSON-представление value.*/
