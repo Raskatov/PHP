688d9ab8cc1575cc935013da52a627b4989182a6
Alexandr <dcl1704@yandex.ru>
2018-03-03 11:33:50 +0400

    Create 02.09.03 Подсчет элементов в массиве.php

---
+++ 02.09.03 Подсчет элементов в массиве.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+count
+int count ( mixed $array_or_countable [, int $mode = COUNT_NORMAL ] )
+/*Подсчитывает количество элементов массива
+Если необязательный параметр mode установлен в
+COUNT_RECURSIVE (или 1), count() будет рекурсивно
+подсчитывать количество элементов массива. Это полезно для
+подсчёта всех элементов многомерных массивов.*/
+
+
+
+
+array_sum
+number array_sum ( array $array )
+/*array_sum() возвращает сумму
+значений массива.*/
+
+
+
+
+array_product
+number array_product ( array $array )
+/*array_product() возвращает произведение
+значений массива.*/
