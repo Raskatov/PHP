46c4a4b58ef763da536c01f282953941a9fbf67e
Alexandr <dcl1704@yandex.ru>
2018-03-03 11:32:53 +0400

    Create 02.09.02 Поиск в массиве.php

---
+++ 02.09.02 Поиск в массиве.php
@@ -0,0 +1,24 @@
+<?php
+
+in_array
+bool in_array ( mixed $needle , array $haystack [, bool $strict = FALSE ] )
+/*Ищет в haystack значение needle.
+Если strict не установлен, то при поиске будет использовано
+нестрогое сравнение.*/
+
+
+
+array_search
+mixed array_search ( mixed $needle , array $haystack [, bool $strict = false ] )
+/*Осуществляет поиск данного значения в массиве и возвращает
+соответствующий ключ в случае удачи.
+Эта функция может возвращать как boolean FALSE, так и не-
+boolean значение (ключ), которое приводится к FALSE.*/
+
+
+
+array_key_exists
+bool array_key_exists ( mixed $key , array $array )
+/*Функция array_key_exists() возвращает TRUE, если в массиве
+присутствует указанный ключ key. Параметр key может быть
+любым значением, которое подходит для индекса массива.*/
